17c357fe0639cea1888ab2786c73de267e2b377a
Naveen MV <naveen.mv@freecharge.com>
2021-10-14 04:09:56 +0530

    Add TODOs

--- main.js
+++ main.js
@@ -27,3 +27,18 @@ app.render()
     .render()
     .redo()
     .render()
+
+/*
+TO DO:
+1. app.addTransaction({date: DATE, payee: STRING, categoryId: ID, memo: STRING, outflow: NUM, inflow: NUM, cleared: BOOL})
+2. app.moveMoney{{fromId: ID, toId: ID}} // From a category to another category
+3. Logic to delete a category or categoryGroup
+    When deleting a category with atleast a single transaction, 
+    (Or a categoryGroup containing such a category)
+    we need to provide another category as replacement which will get all the transactions 
+    and assinged amount of the deleted group
+4. Change assignMoney logic to consider the month in which it is assigned
+    Each category has a distinct asssign for each month
+    Assigning money should be considered just like transactions and stored in db
+
+*/
