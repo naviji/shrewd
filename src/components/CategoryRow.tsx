0ccfe9c08fe286b007d980af8e630cda2139361d
Naveen MV <naveen.mv@freecharge.com>
2022-03-12 23:40:12 +0530

    Spacing working

--- src/components/CategoryRow.tsx
+++ src/components/CategoryRow.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import TableRow from '@mui/material/TableRow'
 import TableCell from '@mui/material/TableCell'
 import Checkbox from '@mui/material/Checkbox'
-import { IconButton } from '@mui/material'
+import { IconButton, Typography } from '@mui/material'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
 
 interface CategoryData {
   name: String,
@@ -16,20 +19,35 @@ interface CategoryRowProps {
     data: CategoryData;
 }
 
+const MoneyCell = ({ amount }) => {
+  return (
+    <Box sx={{ flexGrow: 1, minWidth: '20%', paddingTop: '8px' }}>
+    <Typography align='center' sx={{ display: 'inline' }} variant='body1' noWrap>
+          {amount}
+    </Typography>
+  </Box>
+  )
+}
 const CategoryRow = ({ data }: CategoryRowProps) => {
   return (
-      <TableRow sx={{ }}>
-              <TableCell>
-              <Checkbox />
-              <IconButton color="inherit">
+    <Box sx={{ display: 'flex', width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Box sx={{ flexGrow: 5, minWidth: '400px' }}>
+            <Checkbox />
+            <IconButton sx={{ visibility: 'visible' }} color="inherit">
               <ArrowDropDownIcon />
-              </ IconButton>
+            </ IconButton>
+            <Box sx={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                <Typography align='left' sx={{ display: 'inline-block', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', maxWidth: '300px' }} variant='body1'>
                 {data.name}
-              </TableCell>
-              <TableCell align="right">{data.budgeted}</TableCell>
-              <TableCell align="right">{data.spent}</TableCell>
-              <TableCell align="right">{data.balance}</TableCell>
-            </TableRow>
+              </Typography>
+            </Box>
+
+        </Box>
+
+        <MoneyCell amount={data.budgeted} />
+        <MoneyCell amount={data.spent} />
+        <MoneyCell amount={data.balance} />
+    </Box>
   )
 }
 
