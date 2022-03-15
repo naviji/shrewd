3b67281f054ecfe43dd031ad610f4600d5f08e9a
Naveen MV <naveen.mv@freecharge.com>
2022-03-15 15:16:10 +0530

    cp

--- src/components/CategoryRow.tsx
+++ src/components/CategoryRow.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState, useRef, forwardRef, useEffect } from 'react'
 import Checkbox from '@mui/material/Checkbox'
-import { IconButton, Typography } from '@mui/material'
+import { IconButton, Typography, ButtonBase, TextField } from '@mui/material'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import Box from '@mui/material/Box'
 import MoneyCell from './MoneyCell'
+import CssBaseline from '@mui/material/CssBaseline'
+import ClickAwayListener from '@mui/material/ClickAwayListener'
 
 /*
-
-Text align and padding of input need to be done using global css styles.
-https://mui.com/api/input/
-https://mui.com/guides/interoperability/#global-css
-https://mui.com/customization/theme-components/#global-style-overrides
+  Text align and padding of input need to be done using global css styles.
+  https://mui.com/api/input/
+  https://mui.com/guides/interoperability/#global-css
+  https://mui.com/customization/theme-components/#global-style-overrides
 */
 
 interface CategoryRowProps {
@@ -19,12 +20,119 @@ interface CategoryRowProps {
   spent: number,
   balance: number,
   saveBudgetedAmount: Function,
+  saveCategoryName: Function,
   isGroup: Boolean
 }
 
-const CategoryRow = ({ name, budgeted, spent, balance, saveBudgetedAmount, isGroup }: CategoryRowProps) => {
+const CategoryNameUneditable = ({ name }: any) => {
+  return (
+    <ButtonBase>
+      <Typography align='left' sx={{ display: 'inline-block', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', maxWidth: '250px', verticalAlign: 'middle' }} variant='body1'>
+        {name}
+      </Typography>
+    </ButtonBase>
+  )
+}
+
+const CategoryNameEditable = forwardRef((props: any, ref) => {
+  const { name, setClickedFalse, saveCategoryName } = props
+  const [tempName, setTempName] = useState(name)
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    if (inputRef) {
+      inputRef.current.focus()
+    }
+  }, [])
+
+  return (
+    <Box
+      component="form" noValidate
+      autoComplete="off"
+      sx={{
+        '& > :not(style)': { m: 0 },
+        display: 'inline-block',
+        cursor: 'text',
+        border: 'solid #4495d7',
+        borderRadius: '8px',
+        width: '300px'
+      }}>
+        <TextField variant="standard" margin="none" value={tempName}
+            sx={{
+              '& > input': {
+                margin: '0px',
+                paddingTop: 0,
+                paddingBottom: 0
+              },
+              background: '#FFFFFF'
+            }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setTempName(event.target.value)
+            }}
+            onKeyPress={(event: React.KeyboardEvent<HTMLInputElement>) => {
+              if (event.key === 'Enter') {
+                setClickedFalse()
+                event.preventDefault()
+              }
+            }}
+            inputRef={inputRef}
+            onBlur={() => {
+              saveCategoryName(tempName)
+              setClickedFalse()
+            }}
+            style={{ width: '100%' }}
+            InputProps={{
+              disableUnderline: true
+            }}
+        />
+    </Box>
+  )
+})
+CategoryNameEditable.displayName = 'CategoryNameEditable'
+
+const CategoryNameDisplay = ({ name, saveCategoryName } : any) => {
+  const [clicked, setClicked] = useState(false)
+  const [hoveredOver, setHoveredOver] = useState(false)
+  const inputRef = useRef(null)
+
+  const onClickHandler = () => {
+    setClicked(true)
+    // if (inputRef.current) {
+    //   inputRef.current.select()
+    // }
+  }
+
+  const onClickAwayHandler = () => {
+    setClicked(false)
+    // if (inputRef.current) {
+    //   inputRef.current.select()
+    // }
+  }
+
+  return (
+    <React.Fragment>
+    <ClickAwayListener onClickAway={onClickAwayHandler}>
+    <Box
+      onClick={onClickHandler}
+      onMouseEnter={() => setHoveredOver(true)}
+      onMouseLeave={() => setHoveredOver(false)}
+      sx={{ display: 'inline-block', verticalAlign: 'middle', '&:hover': { cursor: 'text', textDecoration: 'underline' } }}>
+        {
+          (clicked)
+            ? <CategoryNameEditable name={name} setClickedFalse={() => setClicked(false)} saveCategoryName={saveCategoryName} />
+            : <CategoryNameUneditable name={name} />
+        }
+      </Box>
+  </ClickAwayListener>
+  </React.Fragment>
+  )
+}
+
+const CategoryRow = ({ name, budgeted, spent, balance, saveBudgetedAmount, saveCategoryName, isGroup }: CategoryRowProps) => {
   const bgColor = isGroup ? '#F0F0F0' : '#FFFFFF'
   return (
+    <React.Fragment>
+    <CssBaseline />
     <Box sx={{
       display: 'flex',
       width: '100%',
@@ -40,16 +148,13 @@ const CategoryRow = ({ name, budgeted, spent, balance, saveBudgetedAmount, isGro
           <IconButton sx={{ visibility: isGroup ? 'visible' : 'hidden' }} color="inherit">
             <ArrowDropDownIcon />
           </ IconButton>
-          <Box sx={{ display: 'inline-block', verticalAlign: 'middle' }}>
-            <Typography align='left' sx={{ display: 'inline-block', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', maxWidth: '250px', verticalAlign: 'middle' }} variant='body1'>
-              {name}
-            </Typography>
-          </Box>
+          <CategoryNameDisplay name={name} saveCategoryName={saveCategoryName}/>
         </Box>
       <MoneyCell editable={true} colored={false} amount={budgeted} saveChangedAmount={saveBudgetedAmount}/>
       <MoneyCell editable={false} colored={false} amount={spent} saveChangedAmount={() => null}/>
       <MoneyCell editable={false} colored={true} amount={balance} saveChangedAmount={() => null} />
     </Box>
+    </React.Fragment>
   )
 }
 
--- src/components/__fixtures__/CategoryRow/CategoryRow.fixture.tsx
+++ src/components/__fixtures__/CategoryRow/CategoryRow.fixture.tsx
@@ -12,45 +12,53 @@ import { useValue } from 'react-cosmos/fixture'
 export default {
   default: () => {
     const [budgeted, setBudgeted] = useValue('budgeted', { defaultValue: 12345600 })
+    const [categoryName, setCategoryName] = useValue('categoryName', { defaultValue: 'Test' })
     return (<CategoryRow
-      name={'True expenses'}
+      name={categoryName}
       spent={12345600}
       balance={12345600}
       budgeted={budgeted}
       saveBudgetedAmount={(v) => setBudgeted(v)}
+      saveCategoryName={(v) => setCategoryName(v)}
       isGroup={false} />
     )
   },
   longName: () => {
     const [budgeted, setBudgeted] = useValue('budgeted', { defaultValue: 12345600 })
+    const [categoryName, setCategoryName] = useValue('categoryName', { defaultValue: 'sklfjldksjflkdsjfldksjflsdkfjskdjfldskjfldksjf' })
     return (<CategoryRow
-      name={'sklfjldksjflkdsjfldksjflsdkfjskdjfldskjfldksjf'}
+      name={categoryName}
       spent={12345600}
       balance={12345600}
       budgeted={budgeted}
       saveBudgetedAmount={(v) => setBudgeted(v)}
+      saveCategoryName={(v) => setCategoryName(v)}
       isGroup={false} />
     )
   },
   hugeAmount: () => {
     const [budgeted, setBudgeted] = useValue('budgeted', { defaultValue: 123412345600 })
+    const [categoryName, setCategoryName] = useValue('categoryName', { defaultValue: 'sklfjldksjflkdsjfldksjflsdkfjskdjfldskjfldksjf' })
     return (<CategoryRow
-      name={'sklfjldksjflkdsjfldksjflsdkfjskdjfldskjfldksjf'}
+      name={categoryName}
       spent={123412345600}
       balance={123412345600}
       budgeted={budgeted}
       saveBudgetedAmount={(v) => setBudgeted(v)}
+      saveCategoryName={(v) => setCategoryName(v)}
       isGroup={false} />
     )
   },
   groupRow: () => {
     const [budgeted, setBudgeted] = useValue('budgeted', { defaultValue: 123412345600 })
+    const [categoryName, setCategoryName] = useValue('categoryName', { defaultValue: 'sklfjldksjflkdsjfldksjflsdkfjskdjfldskjfldksjf' })
     return (<CategoryRow
-      name={'sklfjldksjflkdsjfldksjflsdkfjskdjfldskjfldksjf'}
+      name={categoryName}
       spent={123412345600}
       balance={123412345600}
       budgeted={budgeted}
       saveBudgetedAmount={(v) => setBudgeted(v)}
+      saveCategoryName={(v) => setCategoryName(v)}
       isGroup={true} />
     )
   }
